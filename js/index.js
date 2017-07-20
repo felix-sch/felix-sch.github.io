e5291fd9e5f21d8c29ee831afe18f4f2a3bebd76
Felix Schaumann <felix.schaumann@annalect.com>
2017-07-20 09:34:13 +0200

    Add of cocktails sources

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $("#navContainer").load("nav.html");
+  $("#searchContainer").load("search.html");
+  $("#sliderContainer").load("slider.html");
+
+  $(document).on('click', 'a.page-scroll', function(event) {
+    var $ele = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($ele.attr('href')).offset().top - 60)
+    }, 600);
+    event.preventDefault();
+  });
+});
+jQuery(document).ready(function() {
+  jQuery(window).scroll(function() {
+    var navbar = jQuery("#navContainer").offset().top;
+    var scrollpos = jQuery(window).scrollTop();
+    if (scrollpos >= navbar) {
+      jQuery("nav").addClass("navbar-fixed-top");
+    } else {
+      jQuery("nav").removeClass("navbar-fixed-top");
+    }
+  });
+});
